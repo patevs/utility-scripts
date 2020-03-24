92fb148b86c68af1addeea668ebf258eb10cece5
PatEvs <ppevans11@gmail.com>
2020-03-24 14:23:14 +1300

    ✨  Updated git/installs/git-extras.ps1

--- scripts/git/installs/git-extras.ps1
+++ scripts/git/installs/git-extras.ps1
@@ -1,6 +1,28 @@
+<#
 # ----------------------------------- #
 # scripts/git/installs/git-extras.ps1 #
 # ----------------------------------- #
+
+.SYNOPSIS
+  PowerShell script used to install git-extras utilities.
+
+.DESCRIPTION
+  Ensures git is installed then finds the git executable
+  location (first path in where git.exe wins) on disk.
+  Finally clones the git-extras repository and runs the
+  provided install.cmd script.
+
+.EXAMPLE
+  ./git-extras
+
+.NOTES
+  File Name: git-extras.ps1
+  Author: PatEvs
+  Last Edit: 24/03/2020
+
+.LINK
+  https://github.com/tj/git-extras
+#>
 
 #
 
