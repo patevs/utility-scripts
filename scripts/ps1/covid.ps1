ae29804ccecfc3731b3f18a1436e3b1bec634c1a
PatEvs <ppevans11@gmail.com>
2020-04-03 17:26:14 +1300

    ✨  Updated ps1/covid.ps1

---
+++ scripts/ps1/covid.ps1
@@ -0,0 +1,32 @@
+<# scripts/ps1/covid.ps1
+
+.SYNOPSIS
+  PowerShell script for tracking the COVID19 pandemic.
+
+  ! Requires bash and curl to be installed.
+
+.DESCRIPTION
+  Command line tracker for the Novel Coronavirus or COVID19 pandemic
+
+  **NOTE** : This script will install the [`PSWriteColor`](https://github.com/EvotecIT/PSWriteColor) module.
+
+.EXAMPLE
+  .\covid.ps1
+
+.NOTES
+  File Name: covid.ps1
+  Author: PatEvs (https://github.com/patevs)
+  Last Edit: 03/04/2020 - April 3rd 2020
+
+.LINK
+  Repository:
+    * https://github.com/patevs/utility-scripts
+  Script:
+    * https://github.com/patevs/utility-scripts/blob/master/scripts/ps1/covid.ps1
+#>
+
+# ------------------------------------------------------------------------------------------- #
+
+#
+
+# EOF #
