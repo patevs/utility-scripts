02b85186cf5e8c0dd1da776cb454c0a14767ae26
Patrick Evans <ppevans11@gmail.com>
2020-04-28 13:18:12 +1200

    :sparkles: Created ps1/enable-wsl.ps1

---
+++ ps1/enable-wsl.ps1
@@ -0,0 +1,9 @@
+# -------------- #
+# enable-wsl.ps1 #
+# -------------- #
+
+# ! NOTE: Must be run from an elevated prompt
+
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+# EOF #
