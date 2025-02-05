87203591d591cedc3f95f1a0b689e462c7cf9228
Patrick Evans <ppevans11@gmail.com>
2025-02-05 15:40:15 +1300

    :tada: Add `cmd/InstallPrinter.cmd`

---
+++ cmd/InstallPrinter.cmd
@@ -0,0 +1,8 @@
+:: InstallPrinter.cmd
+:: ------------------
+
+:: Run from elevated command prompt.
+::
+:: Source: https://www.reddit.com/r/sysadmin/comments/r80q8l/we_cant_install_this_printer_right_now_error_740/
+
+rundll32 printui.dll,PrintUIEntry /il
